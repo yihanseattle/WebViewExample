4f90d19c322546bf1120ee13635d7e176d75d68d
yihanseattle <yihanseattle@gmail.com>
2017-03-17 16:57:30 -0700

    Add Dialog testing.

--- app/src/main/java/com/example/yihan/mywebviewapp/MainActivity.java
+++ app/src/main/java/com/example/yihan/mywebviewapp/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.yihan.mywebviewapp;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String APP_TITLE = "My App";
     private WebView mWebView;
 
     Context mContext;
@@ -40,6 +51,49 @@ public class MainActivity extends AppCompatActivity {
         // Stop local links and redirects from opening in browser instead of WebView
         mWebView.setWebViewClient(new MyAppWebViewClient());
 
+        showDialog();
+
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set title
+        alertDialogBuilder.setTitle("Your Title");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("Click yes to exit!")
+                .setCancelable(false)
+                .setPositiveButton("Yes1234567890",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+//                        MainActivity.this.finish();
+                        Toast.makeText(mContext, "Yes", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No1234567890",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+//                        dialog.cancel();
+                        Toast.makeText(mContext, "No", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNeutralButton("Maybe1234567890", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(mContext, "Maybe", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
     }
 
 }
